114cc46e72a8d785503f04e5289750a0d3f1d1e7
Juho Eskeli <juho.eskeli@alifsemi.com>
2026-01-15 15:41:51 +0200

    compiles etc.

--- samples/modules/tflite-micro/alif_object_detection/src/application/api/use_case/alif_ui/src/Alif240_white.c
+++ samples/modules/tflite-micro/alif_object_detection/src/application/api/use_case/alif_ui/src/Alif240_white.c
@@ -369,11 +369,13 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_ALIF240_W
 };
 
 const lv_img_dsc_t Alif240_white = {
-  .header.cf = LV_IMG_CF_INDEXED_8BIT,
-  .header.always_zero = 0,
-  .header.reserved = 0,
+  .header.magic = LV_IMAGE_HEADER_MAGIC,
+  //.header.cf = LV_IMG_CF_INDEXED_8BIT,
+  .header.cf = LV_COLOR_FORMAT_I8,
+  .header.reserved_2 = 0,
   .header.w = 240,
   .header.h = 87,
+  .header.stride = 240,
   .data_size = 21904,
   .data = Alif240_white_map,
 };
